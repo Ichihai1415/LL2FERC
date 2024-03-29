ef0f2754c0bc46250fbf96d3d8adc6800f2d4293
Ichihai1415 <projects3141.gif@gmail.com>
2024-03-29 14:01:15 +0900

    テストプロジェクト

---
+++ LL2FERC.Test/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace LL2FERC.Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //create csv
+            string csv1 = "code,name\n";
+            string csv2 = "code,name\n";
+            string csv3 = "code,name\n";
+            for (int i = 1; i <= 757; i++)
+            {
+                csv1 += $"{i},\n";
+                csv2 += $"{i},{LL2FERC.GetName_ja(i)}\n";
+                csv3 += $"{i},{LL2FERC.GetName_enUS(i)}\n";
+            }
+            File.WriteAllText("namelist_file.csv", csv1);
+            File.WriteAllText("namelist_ja.csv", csv2);
+            File.WriteAllText("namelist_en-US.csv", csv3);
+
+        }
+    }
+}
